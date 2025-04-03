2d37fbdca60bea939c8a3028dc1e1b50114801e0
tiansikai <1582595806@qq.com>
2025-04-03 14:40:09 +0800

    feat: 实现悬浮窗口拖动手势
    - 在 'BaseFloatingComposeView' 中添加了拖动手势检测，允许用户拖动悬浮窗口。
    - 实现了 'animateTo' 函数，以在拖动时更新窗口位置，并确保其保持在屏幕边界内。
    - 实现了 'returnToTheEdgeOfTheScreen' 函数，以在释放拖动时，将悬浮窗口动画移动到屏幕边缘。
    - 更新了 'StarterFloatingComposeView' 以移除其自身的拖动手势逻辑，转而使用 'BaseFloatingComposeView' 中实现的新功能。
    - 使用 'rememberCoroutineScope' 来管理协程作用域，以进行拖动动画和位置更新。

--- app/src/main/java/com/aking/starter/screens/floating/StarterFloatingComposeView.kt
+++ app/src/main/java/com/aking/starter/screens/floating/StarterFloatingComposeView.kt
@@ -3,7 +3,6 @@ package com.aking.starter.screens.floating
 import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
@@ -14,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import com.aking.starter.ui.views.BaseFloatingComposeView
 
@@ -31,15 +29,7 @@ class StarterFloatingComposeView(context: Context) : BaseFloatingComposeView(con
             Modifier
                 .size(100.dp)
                 .background(Color.Blue)
-                .pointerInput(Unit) {
-                    detectDragGestures(onDragEnd = {
-                        returnToTheEdgeOfTheScreen()
-                    }) { change, dragAmount ->
-                        windowParams.x = (windowParams.x + dragAmount.x.toInt()).coerceIn(0, screenSize.x.toInt())
-                        windowParams.y = (windowParams.y + dragAmount.y.toInt()).coerceIn(0, screenSize.y.toInt())
-                        windowManager.updateViewLayout(this@StarterFloatingComposeView, windowParams)
-                    }
-                }) {
+        ) {
             Text(
                 text, Modifier
                     .background(Color.Gray)
--- app/src/main/java/com/aking/starter/ui/views/BaseFloatingComposeView.kt
+++ app/src/main/java/com/aking/starter/ui/views/BaseFloatingComposeView.kt
@@ -5,15 +5,26 @@ import android.os.Build
 import android.view.Gravity
 import android.view.WindowManager
 import android.widget.FrameLayout
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.platform.compositionContext
+import androidx.compose.ui.unit.IntOffset
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewModelStoreOwner
 import com.aking.starter.utils.getScreenSize
+import kotlinx.coroutines.launch
 
 /**
  * @author Ak
@@ -31,11 +42,32 @@ abstract class BaseFloatingComposeView(context: Context) : FrameLayout(context),
 
     override val lifecycle: Lifecycle get() = viewTreeOwners.lifecycle
 
+    /** 屏幕尺寸 */
+    protected val screenSize by lazy { windowManager.getScreenSize() }
+
+    private var targetAnimateX = 0
+    private var targetAnimateY = 0
+
+    /** 拖拽偏移量动画 */
+    private val animOffset = Animatable(IntOffset(0, 0), IntOffset.VectorConverter)
+
     init {
         this.addView(ComposeView(context).apply {
             compositionContext = viewTreeOwners.reComposer
             setViewCompositionStrategy(ViewCompositionStrategy.Default)
-            setContent { this@BaseFloatingComposeView.Content() }
+            setContent {
+                val coroutineScope = rememberCoroutineScope()
+                Box(modifier = Modifier.pointerInput(Unit) {
+                    detectDragGestures(onDragStart = {
+                        targetAnimateX = windowParams.x
+                        targetAnimateY = windowParams.y
+                    }, onDragEnd = {
+                        coroutineScope.launch { returnToTheEdgeOfTheScreen() }
+                    }) { _, dragAmount ->
+                        coroutineScope.launch { animateTo(dragAmount) }
+                    }
+                }) { this@BaseFloatingComposeView.Content() }
+            }
         })
         this.addOnAttachStateChangeListener(viewTreeOwners)
     }
@@ -84,19 +116,31 @@ abstract class BaseFloatingComposeView(context: Context) : FrameLayout(context),
         flags = flags or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
     }
 
-    /** 屏幕尺寸 */
-    protected val screenSize by lazy { windowManager.getScreenSize() }
-
-    protected fun returnToTheEdgeOfTheScreen() {
-        //悬浮窗的中心点
-        val centerX = (2 * windowParams.x + width) / 2
-        //在屏幕的左侧，位移到0
-        if (centerX < screenSize.x / 2f) {
-            windowParams.x = 0
-        } else {
-            //在屏幕右侧，位移到屏幕宽度-自身宽度（保证右侧贴屏幕）
-            windowParams.x = (screenSize.x - width).toInt()
+    /**
+     * 悬浮窗拖拽
+     */
+    private suspend fun animateTo(dragAmount: Offset) {
+        targetAnimateX = (targetAnimateX + dragAmount.x.toInt()).coerceIn(0, screenSize.x.toInt())
+        targetAnimateY = (targetAnimateY + dragAmount.y.toInt()).coerceIn(0, screenSize.y.toInt())
+        animOffset.animateTo(IntOffset(targetAnimateX, targetAnimateY)) {
+            windowParams.x = value.x
+            windowParams.y = value.y
+            windowManager.updateViewLayout(this@BaseFloatingComposeView, windowParams)
         }
-        windowManager.updateViewLayout(this, windowParams)
+    }
+
+    /**
+     * 松手时，返回到屏幕边缘
+     */
+    private suspend fun returnToTheEdgeOfTheScreen() {
+        //悬浮窗的中心点
+        val centerX = targetAnimateX + width / 2
+        //在屏幕的左侧，位移到0
+        //在屏幕右侧，位移到屏幕宽度-自身宽度（保证右侧贴屏幕）
+        val x = if (centerX < screenSize.x / 2f) 0 else (screenSize.x - width).toInt()
+        animOffset.animateTo(IntOffset(x, targetAnimateY), tween()) {
+            windowParams.x = value.x
+            windowManager.updateViewLayout(this@BaseFloatingComposeView, windowParams)
+        }
     }
 }
